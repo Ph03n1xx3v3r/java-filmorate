0064f6b64b4d853f90f6b5fd74d952131c7f87e9
Ph03n1xx <averin.s.v@yandex.ru>
2026-08-30 02:22:12 +0300

    fix: add empty lines between methods in storage interfaces (Checkstyle)

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/FilmStorage.java
@@ -5,12 +5,20 @@ import java.util.List;
 import java.util.Optional;
 
 public interface FilmStorage {
+
     List<Film> getAllFilms();
+
     Optional<Film> getFilm(Long id);
+
     Film addFilm(Film film);
+
     Film updateFilm(Film film);
+
     void deleteFilm(Long id);
+
     void addLike(Long filmId, Long userId);
+
     void removeLike(Long filmId, Long userId);
+
     List<Film> getPopular(int count);
 }
--- src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/user/UserStorage.java
@@ -5,13 +5,22 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserStorage {
+
     List<User> getAllUsers();
+
     Optional<User> getUser(Long id);
+
     User addUser(User user);
+
     User updateUser(User user);
+
     void deleteUser(Long id);
+
     void addFriend(Long userId, Long friendId);
+
     void removeFriend(Long userId, Long friendId);
+
     List<User> getFriends(Long userId);
+
     List<User> getCommonFriends(Long userId, Long otherId);
 }
