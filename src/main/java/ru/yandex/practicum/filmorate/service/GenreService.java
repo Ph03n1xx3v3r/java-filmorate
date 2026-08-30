c5adaaa2a9e2834ac680f497f16a240c4085c7c6
Ph03n1xx <averin.s.v@yandex.ru>
2026-08-30 03:29:39 +0300

    Откат

--- src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
+++ src/main/java/ru/yandex/practicum/filmorate/service/GenreService.java
@@ -1,34 +1,34 @@
 package ru.yandex.practicum.filmorate.service;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 import ru.yandex.practicum.filmorate.exception.NotFoundException;
 import ru.yandex.practicum.filmorate.model.Genre;
 
 import java.util.List;
-import java.util.Map;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class GenreService {
-    private final Map<Integer, Genre> genres = Map.of(
-            1, new Genre(1, "Комедия"),
-            2, new Genre(2, "Драма"),
-            3, new Genre(3, "Мультфильм"),
-            4, new Genre(4, "Триллер"),
-            5, new Genre(5, "Мелодрама"),
-            6, new Genre(6, "Фантастика")
-    );
+    private final JdbcTemplate jdbcTemplate;
+    private final RowMapper<Genre> genreRowMapper = (rs, rowNum) ->
+            new Genre(rs.getInt("id"), rs.getString("name"));
 
     public List<Genre> getAllGenres() {
-        return List.copyOf(genres.values());
+        String sql = "SELECT * FROM genres ORDER BY id";
+        return jdbcTemplate.query(sql, genreRowMapper);
     }
 
     public Genre getGenre(int id) {
-        Genre genre = genres.get(id);
-        if (genre == null) {
+        String sql = "SELECT * FROM genres WHERE id = ?";
+        List<Genre> result = jdbcTemplate.query(sql, genreRowMapper, id);
+        if (result.isEmpty()) {
             throw new NotFoundException("Жанр с id " + id + " не найден");
         }
-        return genre;
+        return result.get(0);
     }
 }
